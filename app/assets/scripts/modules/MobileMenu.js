cf21e9ff6dcc7c946fc90267b5ee52989b040378
Kevin Shrestha <kevin.g.shrestha@gmail.com>
2020-01-16 09:10:03 -0500

    completed desktop and mobile header styles

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -1,32 +1,20 @@
 class MobileMenu {
   constructor() {
-    //clean
     this.menuIcon = document.querySelector(".site-header__menu-icon")
+    this.menuContent = document.querySelector(".site-header__menu-content")
+    this.siteHeader = document.querySelector(".site-header")
+    this.events()
   }
-  //messy
-  // document.querySelector(".site-header__menu-icon").addEventListener("click", function () {
-  //   console.log("The top right icon was clicked.");
-  // }) 
 
-  // arrow function, it does not manipulate the value of the keyword.
-  // event listener is not receiving a direct reference to our method
-  // instead we're passing out event listen arrow function that it's going to call 
-  // and then it's our arrow function that will execute "toggleTheMenu"
-  // since an arrow funciton does not manipulate "this" keyword
   events() {
     this.menuIcon.addEventListener("click", () => this.toggleTheMenu())
-    // if we did not provide an arrow function, instead listed a direct reference to toggle
-    // then addEventListener would hae ran the code below within a context where it would be
-    // pointing towards the elements that got clicked on
-    // because we provided and arrow function, "this" keyboard doesn't change
-  }
-  // when this code runs the "this" keyword, it will still be pointing towards our overall object
-  toggleTheMenu() {
-    // when this line of code runs, this is still pointing towards our overall object
-    // which lets us easily access our menu content property
-    this.menuContent.classList.toggle("site-header__menu-content--is-visible")
   }
 
+  toggleTheMenu() {
+    this.menuContent.classList.toggle("site-header__menu-content--is-visible")
+    this.siteHeader.classList.toggle("site-header--is-expanded")
+    this.menuIcon.classList.toggle("site-header__menu-icon--close-x")
+  }
 }
 
 export default MobileMenu
