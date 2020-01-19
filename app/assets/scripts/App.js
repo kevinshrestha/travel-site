fc4df920a03bf86582d1751f32f19333b0045d11
Kevin Shrestha <kevin.g.shrestha@gmail.com>
2020-01-18 23:20:00 -0500

    Completed sticky header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,9 +1,11 @@
 import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
+import StickyHeader from './modules/StickyHeader'
 
 //let is the new instance of the class
 let mobileMenu = new MobileMenu();
+let stickyHeader = new StickyHeader();
 
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
