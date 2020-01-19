07f89e26d548ba55f30b3c50ecd75817dd75fc78
Kevin Shrestha <kevin.g.shrestha@gmail.com>
2020-01-19 13:35:03 -0500

    Completed Modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -4,11 +4,28 @@ import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
 
 //let is the new instance of the class
-let mobileMenu = new MobileMenu();
-let stickyHeader = new StickyHeader();
-
+new MobileMenu();
+new StickyHeader();
+// using let, get in the habit of storing these into variables if your different modules would need to interact with each other
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
+// creating a variable with this name in the global scope
+let modal
+
+document.querySelectorAll(".open-modal").forEach(el => {
+  el.addEventListener("click", e => {
+    e.preventDefault()
+    if (typeof modal == "undefined") {
+      // x represents the file we imported
+      import(/* webpackChunkName: "modal" */ './modules/Modal').then(x => {
+        modal = new x.default()
+        setTimeout(() => modal.openTheModal(), 20)
+      }).catch(() => console.log("There was a problem"))
+    } else {
+      modal.openTheModal()
+    }
+  })
+})
 
 if (module.hot) {
   module.hot.accept()
